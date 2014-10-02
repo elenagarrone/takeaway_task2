41d115ae20646d2a85afc31eff6d13a30317263f
elena <elena.15@hotmail.it>
2014-10-02 19:43:56 +0100

    added customer

---
+++ lib/customer.rb
@@ -0,0 +1,25 @@
+require_relative 'order'
+
+class Customer
+
+	attr_reader :name, :phone, :order
+
+	def initialize(name, phone)
+		@name = name
+		@phone = phone
+		@order = Order.new
+	end
+
+	def choose(item)
+		@order.add(item)
+	end
+
+	def delete(item)
+		@order.remove(item)
+	end
+
+	def send_order
+		@order.submitted_order
+	end
+
+end
